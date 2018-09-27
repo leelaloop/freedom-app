94b839acc3c8e8288eb800483c4e866c7753be08
James Risberg <james.risberg@gmail.com>
2018-09-26 17:33:02 -0700

    Change firebase code to use Firestore

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,14 @@ import firebase from 'firebase';
 const config = {
     apiKey: "AIzaSyB6e1qzWxg5cyOitKTWONQ-vBx6laYohqQ",
     authDomain: "freedom-4056d.firebaseapp.com",
-    databaseURL: "https://freedom-4056d.firebaseio.com",
     projectId: "freedom-4056d",
-    storageBucket: "freedom-4056d.appspot.com",
-    messagingSenderId: "619590820138"
 };
 
 firebase.initializeApp(config);
+
+var db = firebase.firestore();
+db.settings({
+    timestampsInSnapshots: true
+});
 
 export default firebase;
