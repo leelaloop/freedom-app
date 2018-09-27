1a3ae5c9a2ee8cb46d9267dd7c9ac7e1be22528e
James Risberg <james.risberg@gmail.com>
2018-09-26 17:33:21 -0700

    Hook add location form to firebase, add fields for lat and long

--- src/AddLocationForm.js
+++ src/AddLocationForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import firebase from "./firebase.js"
 
 import './AddLocationForm.css';
 
@@ -7,41 +8,85 @@ class AddLocationForm extends Component {
         super();
         this.state = {
             showForm: false,
-            spaceName: '',
+            name: '',
+            latitude: '',
+            longitude: '',
         };
     
         this.handleClick = this.handleClick.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleClick() {
-        this.setState(state => ({
-            showForm: !state.showForm
-        }));
+        this.setState({
+            showForm: !this.state.showForm
+        });
     }
 
     handleChange(e) {
         this.setState({
-          [e.target.name]: e.target.value
+            [e.target.name]: e.target.value
         });
-      }
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        firebase.firestore().collection("locations").add({
+            name: this.state.name,
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+        }).then(function(docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function(error) {
+            console.error("Error adding document: ", error);
+        }); 
+        
+        this.setState({
+            showForm: false,
+            name: '',
+            latitude: '',
+            longitude: '',
+        })
+    }
 
     render() {
         return (
             <div id='formArea'>
-                {/* {
+                {
                     !this.state.showForm ?
                     
-                    <button onClick={this.handleClick}> Button </button> 
+                    <button onClick={this.handleClick}> Add Location </button> 
                     
                     :
                     
-                    <button onClick={this.handleClick}> Form </button>
-                } */}
-                <form>
-                    <input type="text" name="spaceName" placeholder="Name your space..." onChange={this.handleChange} value={this.state.spaceName} />
-                    <button>Add Space</button>
-                </form>
+                    <form onSubmit={this.handleSubmit}>
+                        <input 
+                            type="text" 
+                            name="name" 
+                            placeholder="Name your space..." 
+                            onChange={this.handleChange} 
+                            value={this.state.name} 
+                        />
+                        <input 
+                            type="text" 
+                            name="latitude" 
+                            placeholder="Latitude" 
+                            onChange={this.handleChange} 
+                            value={this.state.latitude} 
+                        />
+                        <input 
+                            type="text" 
+                            name="longitude" 
+                            placeholder="Longitude" 
+                            onChange={this.handleChange} 
+                            value={this.state.longitude} 
+                        />
+                        <button>Add Space</button>
+                    </form>
+                }
             </div>
         )
     }
